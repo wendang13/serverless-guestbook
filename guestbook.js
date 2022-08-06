a1c1bda0e37afdba4c9ccddfb83ee7b55d21c2c5
wendang13 <110578257+wendang13@users.noreply.github.com>
2022-08-06 18:44:38 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://github.com/wendang13/serverless-guestbook.git';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
